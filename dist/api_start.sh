210b06d800fca09c71242f9e4e62e38d63ab2b4c
Sandokaaan <30953425+Sandokaaan@users.noreply.github.com>
2022-11-13 23:41:56 +0100

    Update api_start.sh

--- dist/api_start.sh
+++ dist/api_start.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+FIFO="/tmp/apififo"
+
+# delete old FIFO
+rm -f $FIFO
 # create new FIFO
-rm -f /tmp/apififo
-mkfifo /tmp/apififo
+mkfifo $FIFO
+
+# wait a moment
+WAIT=$( earthcoin-cli -getinfo | grep protocolversion )
+while [[ -z "$WAIT" ]]
+do
+  sleep 3s
+  WAIT=$( earthcoin-cli -getinfo | grep protocolversion )
+  echo waiting...
+done
+echo ready
 
 # go to installed path
 cd /usr/local/share/apijava/
